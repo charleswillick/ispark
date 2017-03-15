c321ab56305f9eb83fc98f88f62651e50464d2a6
zqwu <zqwu@iflytek.com>
2017-03-15 22:41:11 +0800

    增加单元测试样例

---
+++ kungfu/src/test/scala/happy/istudy/algorithm/bst/BinarySearchTreeTest.scala
@@ -0,0 +1,13 @@
+package happy.istudy.algorithm.bst
+
+import org.scalatest.FunSuite
+
+/**
+  * Created by zqwu on 2017/3/15 0015.
+  */
+class BinarySearchTreeTest extends FunSuite{
+  test("test create function"){
+    assert("hello world" == "hello world")
+  }
+
+}
