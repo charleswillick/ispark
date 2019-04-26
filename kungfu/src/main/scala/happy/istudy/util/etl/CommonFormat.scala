6cffa81ddcca7339318bb7851269c3cc7e0c7d3f
Admin <ahysbgwzq@163.com>
2019-04-26 16:33:14 +0800

    提升jdk版本至1.8，scala 版本2.11.8 spark版本2.3.2

--- kungfu/src/main/scala/happy/istudy/util/etl/CommonFormat.scala
+++ kungfu/src/main/scala/happy/istudy/util/etl/CommonFormat.scala
@@ -1,61 +1,11 @@
 package happy.istudy.util.etl
 
-import happy.istudy.util.keyset.ItemKeys
 import net.liftweb.json._
 
 /**
   * Created by zqwu on 2016/4/26 0026.
   */
 object CommonFormat {
-
-  /**
-    * 提取document日志中channels字段里的channelname, channelid
-    * @param channelsJson
-    * @return
-    */
-  def getChannels(channelsJson:String):Map[String,String] = {
-    implicit val formats = DefaultFormats
-    //case class Channels (channelid:String,parentid:String,channelname:String,desc:String)
-    case class Channels (channelid:String,channelname:String)
-    try{
-      val json = parse(channelsJson)
-      val channels = json.children
-      val firstChannel = channels(0)
-      val m = firstChannel.extract[Channels]
-      Map(ItemKeys.CHANNEL -> m.channelname, ItemKeys.CHANNELID -> m.channelid)
-    }catch {
-      case ex:Exception => Map[String,String]()
-    }
-
-  }
-
-  /**
-    * 获取新闻日志中需要的字段
-    * @param record ossp刷来的原始日志（flume,kafka）
-    * @return 需要提取的的字段
-    */
-  def itemTransform(record: Map[String, String]):Map[String,String] = {
-    val channel = if(record.contains("channels")) getChannels(record.get("channels").get)
-    else Map[String,String]()
-    val ret = Map(ItemKeys.DOCID -> record.getOrElse("docid", "00000"),
-      ItemKeys.SOURCE -> record.getOrElse("sourcename","").replaceAll(":",""),
-      ItemKeys.CHANNEL -> channel.getOrElse("channelname",ItemKeys.DEFAULT_CHANNEL),
-      ItemKeys.TITLE -> record.getOrElse("title",""),
-      ItemKeys.RSOURCENAME -> record.getOrElse("rsourcename",""),
-      ItemKeys.CTM -> formatTime(record.getOrElse("ctm","")),
-      ItemKeys.PUBLISHTIME -> formatTime(record.getOrElse("publishtime","")),
-      ItemKeys.COMMENT -> record.getOrElse("comment",""),
-      ItemKeys.UP -> record.getOrElse("up",""),
-      ItemKeys.DOWN -> record.getOrElse("down",""),
-      ItemKeys.LIKE -> record.getOrElse("like",""),
-      ItemKeys.CTYPE -> record.getOrElse("ctype",""),
-      ItemKeys.MODELJSON -> record.getOrElse("contentmodeltags",""),
-      ItemKeys.SAVECONFIG -> record.getOrElse("saveconfig",""),
-      ItemKeys.ALLCONTENT -> record.getOrElse("content",""),
-      ItemKeys.CONTENT -> record.getOrElse("speechdetail", "").toString.replaceAll("~|\\n", " ")
-    ) ++ channel
-    ret
-  }
 
   def formatTime(param:String):String={
     val rparam = param.replaceAll("[^\\d]", "")
--- kungfu/src/main/scala/happy/istudy/util/keyset/LogKeys.scala
+++
@@ -1,9 +0,0 @@
-package happy.istudy.util.keyset
-
-/**
-  * Created by zqwu on 2016/3/23 0023.
-  */
-object LogKeys {
-  val CHANNEL = "channelname"
-
-}
